375cdf471771eff651d66d6b0adc0a5fa4a778d2
Forge <108130903+HappyForge@users.noreply.github.com>
2023-02-21 18:58:33 +0300

    Update guiloader.lua

--- guiloader.lua
+++ guiloader.lua
@@ -1,3 +1,3 @@
 if game.PlaceId == 301549746 then --! Counter Blox
-    loadstring(game:HttpGet"https://raw.githubusercontent.com/HappyForge/hell-gui/main/scripts/Counter%20blox.lua")()
+    loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/HappyForge/hell-gui/main/scripts/Counter%20blox.lua")))()
 end
